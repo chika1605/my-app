9b8d4ada76dd7b3e530b221345ef5d2f5a8f38b8
chika <152640307+chika1605@users.noreply.github.com>
2026-04-03 10:31:05 +0600

    code test -3

--- src/main/java/kg/rubicon/my_app/MyAppApplication.java
+++ src/main/java/kg/rubicon/my_app/MyAppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyAppApplication  {
+public class MyAppApplication игррго {
 
     public static void main(String[] args) {
         SpringApplication.run(MyAppApplication.class, args);
